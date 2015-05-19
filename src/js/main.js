8d3240f27b574c81bec352004f8e4f83c2c17192
makulatura <usilins@gmail.com>
2015-05-19 11:23:09 +0300

    Code reorganized in module

--- src/js/main.js
+++ src/js/main.js
@@ -6,159 +6,164 @@
  * To change this template use File | Settings | File Templates.
  */
 
-function square() {
-    this._isAlive = false;
-    this._neighbours = new Array();
+    //todo automatic generation loop
 
-    function isAlive(){
-        return this._isAlive;
-    }
+(function(){
 
-    function becomeAlive(){
-        this._isAlive = true;
 
-    }
 
-    function becomeDead(){
+var gameOfLife = (function (){
+     var map = new Array();
+     var grid = new Array();
+     var _generation = 0;
+
+    function generation(){ return _generation; };
+
+    function increaseGeneration(){  _generation++; };
+
+    function setMap(userMap){ map = userMap; };
+
+    function square() {
         this._isAlive = false;
-    }
+        this._neighbours = new Array();
 
-    function neighbours(){
-        return this._neighbours;
-    }
+        function isAlive(){ return this._isAlive; }
 
-    function addNeighbour(sqCoords){
-        if(!this._neighbours){
-            this._neighbours = new Array();
+        function becomeAlive(){ this._isAlive = true; }
+
+        function becomeDead(){ this._isAlive = false; }
+
+        function neighbours(){ return this._neighbours; }
+
+        function addNeighbour(sqCoords){
+            if(!this._neighbours){
+                this._neighbours = new Array();
+            }
+            this._neighbours.push(sqCoords);
         }
-        this._neighbours.push(sqCoords);
-    }
 
-    return {
-        isAlive: isAlive,
-        becomeAlive: becomeAlive,
-        becomeDead: becomeDead,
-        neighbours: neighbours,
-        addNeighbour: addNeighbour
-    }
-};
+        return {
+            isAlive: isAlive,
+            becomeAlive: becomeAlive,
+            becomeDead: becomeDead,
+            neighbours: neighbours,
+            addNeighbour: addNeighbour
+        }
+    };
 
 
 
+    function initGame(){
+        var game = new Array();
 
+        for(var i = 0; i < map.length; i++){
+            var row = new Array();
+            for(var j = 0; j < map[0].length; j++){
+                var sq = new square();
 
-function initGame(map){
-    var game = new Array();
+                if(map[i][j] == "x"){
 
-    //todo generation counter
-    //todo module where everything is under game object. For example game.print, game.nextGen, game.grid
+                    sq.becomeAlive();
+                }
 
+                for(var ny = -1; ny <2; ny++){
+                    for(var nx = -1; nx <2; nx++){
+                        var newX = j+ny;
+                        var newY = i+nx;
 
-    for(var i = 0; i < map.length; i++){
-        var row = new Array();
-        for(var j = 0; j < map[0].length; j++){
-            var sq = new square();
-
-            if(map[i][j] == "x"){
-
-                sq.becomeAlive();
-            } /*else {
-                sq.becomeDead();
-            }*/
-            for(var ny = -1; ny <2; ny++){
-                for(var nx = -1; nx <2; nx++){
-                    var newX = j+ny;
-                    var newY = i+nx;
-
-                    //if valid neighbour chosen and not self
-                    if((map[newY] && map[newY][newX]) && ((newX != j) || (newY != i))){
-                        sq.addNeighbour({"x":newX,"y":newY});
+                        //if valid neighbour chosen and not self
+                        if((map[newY] && map[newY][newX]) && ((newX != j) || (newY != i))){
+                            sq.addNeighbour({"x":newX,"y":newY});
+                        }
                     }
                 }
+
+                row.push(sq);
             }
-
-            row.push(sq);
+            game.push(row);
         }
-        game.push(row);
-    }
-
-    return game;
-};
 
 
-function nextGen(game){
-    var nextGen = new Array();
-    //taisām ciklu
-    //pārbaudām katrā šūnā kaimiņus un atgriežam vajadzīgo vērtību nextGenā
-    for(var i = 0; i < game.length; i++){
-        var row = new Array();
-        for(var j = 0; j < game[0].length; j++){
+        grid = game;
+    };
 
-            var cell = Object.create(game[i][j]);
-            var nb = cell.neighbours();
 
-            var liveNeighbours = 0;
-            //skaitām dzīvos kaimiņus
-            for(var k = 0; k < nb.length; k++){
+    function nextGen(){
+        var nextGen = new Array();
+        //taisām ciklu
+        //pārbaudām katrā šūnā kaimiņus un atgriežam vajadzīgo vērtību nextGenā
+        for(var i = 0; i < grid.length; i++){
+            var row = new Array();
+            for(var j = 0; j < grid[0].length; j++){
 
-                var nbx = nb[k].x;
-                var nby = nb[k].y;
-                if(game[nby][nbx].isAlive()){
-                    liveNeighbours++;
+                var cell = Object.create(grid[i][j]);
+                var nb = cell.neighbours();
+
+                var liveNeighbours = 0;
+                //skaitām dzīvos kaimiņus
+                for(var k = 0; k < nb.length; k++){
+
+                    var nbx = nb[k].x;
+                    var nby = nb[k].y;
+                    if(grid[nby][nbx].isAlive()){
+                        liveNeighbours++;
+                    }
                 }
+
+                if(liveNeighbours < 2 || liveNeighbours > 3){
+                    //mirst
+                    cell.becomeDead();
+                } else if(liveNeighbours == 3 && !cell.isAlive()) {
+                    cell.becomeAlive();
+                }
+                row.push(cell);
             }
 
-            if(liveNeighbours < 2){
-                //mirst
-                cell.becomeDead();
-
-            } else if(liveNeighbours > 3){
-                //mirst
-                cell.becomeDead();
-            }else if(liveNeighbours == 3 && !cell.isAlive()) {
-                cell.becomeAlive();
-            }
-            row.push(cell);
+            nextGen.push(row);
         }
 
-        nextGen.push(row);
+
+        increaseGeneration();
+        grid = nextGen;
+    };
+
+
+    function print(){
+
+        var output = "";
+        for(var a = 0; a < grid.length; a++){
+
+            for(var b = 0; b < grid[0].length; b++){
+
+                var res = grid[a][b].isAlive();
+
+                if(res){
+                    output +="x";
+                } else {
+                    output +=".";
+                }
+
+            }
+            output +="\r\n";
+        }
+
+        return output;
     }
 
 
 
-    return nextGen;
-}
 
 
-var print = function(game){
 
-    var output = "";
-    for(var a = 0; a < game.length; a++){
-
-        for(var b = 0; b < game[0].length; b++){
-
-            var res = game[a][b].isAlive();
-
-            if(res){
-                output +="x";
-            } else {
-                output +=".";
-            }
-
-        }
-        output +="\r\n";
+    return {
+        setMap: setMap,
+        initGame: initGame,
+        nextGen: nextGen,
+        print: print,
+        generation: generation
     }
 
-    return output;
-}
-
-
-
-/*var map =  ["      ",
-            "x     ",
-            "x     ",
-            "x     ",
-            "      "];*/
+})();
 
 /*
 
@@ -170,27 +175,20 @@ var print = function(game){
 * */
 
 
-/* testing field */
-/*console.log(print(game));
+    $("#initialize").click(function(){
+        var userMap = $("#userMap").val();
+        var map = userMap.split("\n");
+        gameOfLife.setMap(map);
+        gameOfLife.initGame();
+        $("#lifeMap").html(gameOfLife.print());
 
-game = nextGen(game);
-console.log(print(game));
-game = nextGen(game);
-console.log(print(game));*/
-var game = new Array();
+    });
 
-$("#initialize").click(function(){
-    var userMap = $("#userMap").val();
-    var map = userMap.split("\n");
+    $("#executeGen").click(function(){
+        gameOfLife.nextGen();
+        $("#lifeMap").html(gameOfLife.print());
 
-    game = initGame(map, map[0].length, map.length);
-    $("#lifeMap").html(print(game));
+        $("#gen").text(gameOfLife.generation());
+    });
 
-});
-
-$("#executeGen").click(function(){
-    game = nextGen(game);
-
-    $("#lifeMap").html(print(game));
-
-});
+})();
